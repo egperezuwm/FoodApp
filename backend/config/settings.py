c5c9c4097f5c5954013f0342b8a371457a3c8327
Enrique <egperez@uwm.edu>
2025-02-25 19:21:08 -0600

    replicating FoodDeliveryApp by Israel.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CoresMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 
 ]
 
